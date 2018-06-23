e23fbb1b5b603bcc854a83787837f00fdb4143ee
Nikola Marin <nikolamarin@protonmail.com>
2018-06-23 03:44:04 +0200

    Python

---
+++ Python/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name = 'co2_sensor',
+      version = '1.0',
+      desription = 'CO2 sensor interface',
+      url = '-',
+      author = '-',
+      author_email = '-',
+      licence = '-',
+      packages = ['CO2'],
+      zip_safe = False)
